f3137f30498b114c41d671f224efc7365dc10068
Jack Clifton <outlaw21kawasaki@gmail.com>
2025-12-06 22:27:33 +0000

    server: initial working stage

--- server/sentinel_guardian_server.py
+++ server/sentinel_guardian_server.py
@@ -3,27 +3,51 @@ import RPi.GPIO as GPIO
 import time
 import socket
 import threading
+import struct
+
 
 class Networking:
-    UDP_IP = "192.168a.0.255"
-    UDP_PORT = 5005
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    _alarm_mutex = threading.Lock()
+
+    _sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    class Config:
+        UDP_IP = "192.168.0.255"
+        UDP_PORT = 5005
+
+
+    class Codes:
+        NOTHING = b"nodata"
+        ALARM = b"getfuckingdoorucunts"
+
     
     def init():
-        Networking.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        Networking._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    
+    def send_broadcast_packet(timestamp):
+        packet = struct.pack("!I", int(timestamp)) + Networking.Codes.ALARM
+        Networking._sock.sendto(packet, (Networking.Config.UDP_IP, Networking.Config.UDP_PORT))
+
+
+    def _alarm():
+        if not Networking._alarm_mutex.acquire(blocking=False):
+            return
+
+        timestamp = int(time.time())
+        Networking.send_broadcast_packet(timestamp)
+
+        Networking._alarm_mutex.release()
 
     def alarm():
-        Networking.sock.sendto(MESSAGE, (Networking.UDP_IP, Networking.UDP_PORT))
+        threading.Thread(target=Networking._alarm).start()
 
-
-class DoorCodes:
-    NOTHING = b"nodata"
-    ALARM = b"getfuckingdoorucunts"
 
 
 class GPIOSignal:
 
+    _alarm_mutex = threading.Lock()
     _alarm_stop_event = threading.Event()
 
     class LED:
@@ -33,6 +57,7 @@ class GPIOSignal:
 
     BEEPER = 23
     BUTTON = 24
+
 
     def init():
         GPIO.setmode(GPIO.BCM)
@@ -74,7 +99,10 @@ class GPIOSignal:
         GPIO.output(GPIOSignal.LED.RED, GPIO.HIGH)
     
 
-    def alarm():
+    def _alarm():
+        if not GPIOSignal._alarm_mutex.acquire(blocking=False):
+            return
+
         t_beeper = threading.Thread(target=GPIOSignal._alarm_beeper_thread)
         t_LED = threading.Thread(target=GPIOSignal._alarm_LED_thread)
         t_beeper.start()
@@ -82,8 +110,18 @@ class GPIOSignal:
         t_beeper.join()
         t_LED.join()
         GPIOSignal._alarm_stop_event.clear()
+        GPIOSignal._alarm_mutex.release()
 
 
+    def alarm():
+        threading.Thread(target=GPIOSignal._alarm).start()
+
+
+
+
+def alarm():
+    GPIOSignal.alarm()
+    Networking.alarm()
 
 
 
@@ -97,8 +135,12 @@ def loop():
     time.sleep(0.1)
     
     if GPIO.input(GPIOSignal.BUTTON) == GPIO.LOW:
+        alarm()
 
-        GPIOSignal.alarm()
+        # wait for button to be released
+        while GPIO.input(GPIOSignal.BUTTON) != GPIO.LOW:
+            time.sleep(0.1)
+        
 
 
 
