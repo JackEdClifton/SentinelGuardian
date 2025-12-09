1e665d35b54a277b46a49efcd506015a27e917f7
Jack Clifton <outlaw21kawasaki@gmail.com>
2025-12-09 00:20:52 +0000

    firmware beeper: got bidirectional communication working correctly

--- firmware/beeper/beeper.ino
+++ firmware/beeper/beeper.ino
@@ -5,18 +5,19 @@
 #include "wifi_creds.h"
 
 const int UDP_PORT = 5005;
+const int UDP_RESPONSE_PORT = 5006;
+
+uint32_t g_ts_LAST_ALARM = 0;
 
 WiFiUDP udp;
+WiFiUDP udp_response;
 
 namespace DoorCodes {
 	const char* NOTHING = "nodata";
-	const char* ALARM = "getfuckingdoorucunts";
+	const char* ALARM = "A";
+	const char* CANCEL = "C";
 
-	enum Codes {
-		NO_DATA = 0,
-		START_ALARM,
-		STOP_ALARM
-	};
+	enum Codes { NO_DATA = 0, START_ALARM, STOP_ALARM };
 };
 
 
@@ -28,27 +29,75 @@ const int GPIO_LED_GREEN = 19;
 
 volatile bool g_EVENT_CANCEL_ALARM = false;
 
+unsigned long alarm_prev_millis = 0;
+bool alarm_active = false;
+
 void btn_pressed() {
+	Serial.print("[DEBUG] btn_pressed()\n");
 	g_EVENT_CANCEL_ALARM = true;
 }
 
-void alarm() {
+void start_alarm() {
+
+	Serial.print("[DEBUG] start_alarm() - setting RED LED on\n");
 
 	digitalWrite(GPIO_LED_RED, HIGH);
 	digitalWrite(GPIO_LED_GREEN, LOW);
 
-	while (!g_EVENT_CANCEL_ALARM) {
-		tone(GPIO_ALARM, 100);  // 2kHz tone
-		delay(200);             // tone duration
-		noTone(GPIO_ALARM);     // stop tone
-		delay(70);              // pause between beeps
-	}
+	alarm_prev_millis = millis();
+	Serial.print("[DEBUG] start_alarm() - set alarm_active\n");
+	alarm_active = true;
+	Serial.print("[DEBUG] start_alarm() - unset g_EVENT_CANCEL_ALARM\n");
+	g_EVENT_CANCEL_ALARM = false;
+}
 
+
+void stop_alarm() {
+	Serial.print("[DEBUG] stop_alarm() - disable buzzer & LEDs\n");
+	noTone(GPIO_ALARM);
 	digitalWrite(GPIO_ALARM, LOW);
 	digitalWrite(GPIO_LED_RED, LOW);
 	digitalWrite(GPIO_LED_GREEN, LOW);
 
+	// send cancel packet to server
+	Serial.print("[DEBUG] stop_alarm() - send cancel packet to server\n");
+	udp_response.beginPacket("192.168.0.69", UDP_RESPONSE_PORT);
+	udp_response.print(DoorCodes::CANCEL);
+	udp_response.endPacket();
+
+	Serial.print("[DEBUG] stop_alarm() - unset alarm_active\n");
+	alarm_active = false;
+	Serial.print("[DEBUG] stop_alarm() - unset g_EVENT_CANCEL_ALARM\n");
 	g_EVENT_CANCEL_ALARM = false;
+}
+
+
+void handle_alarm() {
+
+	if (!alarm_active) {
+		return;
+	}
+
+	unsigned long current = millis();
+
+	if (current - alarm_prev_millis >= 270) {
+		// toggle beeper
+		static bool beep_on = false;
+		if (beep_on) {
+			noTone(GPIO_ALARM);
+		}
+		else {
+			tone(GPIO_ALARM, 100);
+		}
+
+		beep_on = !beep_on;
+		alarm_prev_millis = current;
+	}
+
+	if (g_EVENT_CANCEL_ALARM) {
+		Serial.print("[DEBUG] handle_alarm() - g_EVENT_CANCEL_ALARM is set. Calling stop_alarm\n");
+		stop_alarm();
+	}
 }
 
 
@@ -60,49 +109,32 @@ void setup_wifi() {
 
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(WIFI_SSID, WIFI_PASS);
-	Serial.print("Connecting to wifi\n");
+	Serial.print("[DEBUG] setup_wifi() - Connecting to wifi\n");
 
 
-	int ts_start = millis();
-	while (WiFi.status() != WL_CONNECTED) {
-		Serial.print("Not connected to wifi\n");
-		
-		if (millis() - ts_start > WIFI_TIMEOUT_ms) {
-			break;
-		}
-
-		switch (WiFi.status()) {
-			case WL_IDLE_STATUS: Serial.println("WL_IDLE_STATUS"); break;
-			case WL_NO_SSID_AVAIL: Serial.println("WL_NO_SSID_AVAIL"); break;
-			case WL_CONNECT_FAILED:
-				Serial.println("WL_CONNECT_FAILED");
-				break;
-				//		this one is not defined for some reason
-				//			case WL_WRONG_PASSWORD: Serial.println("WL_WRONG_PASSWORD"); break;
-			case WL_DISCONNECTED: Serial.println("WL_DISCONNECTED"); break;
-			case WL_CONNECTED: Serial.println("WL_CONNECTED"); break;
-			default: Serial.println("UNKNOWN");
-		}
+	unsigned long ts_start = millis();
+	while (WiFi.status() != WL_CONNECTED && millis() - ts_start < WIFI_TIMEOUT_ms) {
+		delay(150);
 
 		// heartbeat red LED
-		int interval = digitalRead(GPIO_LED_RED) ? 500 : 500;
+		int interval = digitalRead(GPIO_LED_RED) ? 250 : 250;
 		static unsigned long prev_millis = 0;
 		unsigned long current_millis = millis();
 		if (current_millis - prev_millis >= interval) {
 			prev_millis = current_millis;
 			digitalWrite(GPIO_LED_RED, !digitalRead(GPIO_LED_RED));
 		}
-
-
-		delay(150);
 	}
 
+	Serial.print("[DEBUG] setup_wifi() - Connected to wifi\n");
+
 	udp.begin(UDP_PORT);
+	udp_response.begin(UDP_RESPONSE_PORT);
 }
 
 
 
-int read_parse_udp_packet() {
+DoorCodes::Codes read_parse_udp_packet() {
 
 	// verify data is available
 	int packet_size = udp.parsePacket();
@@ -116,26 +148,40 @@ int read_parse_udp_packet() {
 
 	// verify at least the timestamp is present
 	if (len <= 4) {
+		Serial.print("[DEBUG] read_parse_udp_packet() - insufficient data\n");
 		return DoorCodes::Codes::NO_DATA;
 	}
 
 	// need to convert because of endianness
-	uint32_t timestamp =
-	  (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | (buffer[3]);
+	uint32_t timestamp = (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | (buffer[3]);
+
 
 	// message is directly after the timestamp
 	const char* message = buffer + 4;
 
-	if (strcmp(message, DoorCodes::NOTHING) == 0) {
+	Serial.print("[DEBUG] read_parse_udp_packet() - parsing message\n");
+
+	if (strcmp(message, DoorCodes::ALARM) == 0) {
+		if (timestamp > g_ts_LAST_ALARM) {
+			Serial.print("[DEBUG] read_parse_udp_packet() - new alarm packet recieved\n");
+			g_ts_LAST_ALARM = timestamp;
+			return DoorCodes::Codes::START_ALARM;
+		}
+		else {
+			Serial.print("[DEBUG] read_parse_udp_packet() - repeated alarm packet recieved\n");
+			return DoorCodes::Codes::NO_DATA;
+		}
+	}
+
+	if (strcmp(message, DoorCodes::CANCEL) == 0) {
+		Serial.print("[DEBUG] read_parse_udp_packet() - cancel packet recieved\n");
 		return DoorCodes::Codes::STOP_ALARM;
 	}
 
-	if (strcmp(message, DoorCodes::ALARM) == 0) {
-		return DoorCodes::Codes::START_ALARM;
-	}
-
-	return DoorCodes::Codes::NO_DATA;  // can't be sure
+	Serial.print("[DEBUG] read_parse_udp_packet() - unknown packet, assume no data\n");
+	return DoorCodes::Codes::NO_DATA;
 }
+
 
 
 void setup() {
@@ -161,42 +207,54 @@ void setup() {
 }
 
 
+
 void loop() {
 
 	delay(10);
 
 	// handle wifi
 	if (WiFi.status() == WL_CONNECTED) {
-		digitalWrite(GPIO_LED_RED, LOW);
+		if (!alarm_active) {
+			digitalWrite(GPIO_LED_RED, LOW);
+		}
 	} else {
 		digitalWrite(GPIO_LED_RED, HIGH);
 		digitalWrite(GPIO_LED_GREEN, LOW);
 		setup_wifi();
-		delay(200);
 		return;
 	}
 
 	// heartbeat green LED
-	int interval = digitalRead(GPIO_LED_GREEN) ? 50 : 950;
-	static unsigned long prev_millis = 0;
-	unsigned long current_millis = millis();
-	if (current_millis - prev_millis >= interval) {
-		prev_millis = current_millis;
-		digitalWrite(GPIO_LED_GREEN, !digitalRead(GPIO_LED_GREEN));
+	if (!alarm_active) {
+		int interval = digitalRead(GPIO_LED_GREEN) ? 50 : 950;
+		static unsigned long prev_millis = 0;
+		unsigned long current_millis = millis();
+		if (current_millis - prev_millis >= interval) {
+			prev_millis = current_millis;
+			digitalWrite(GPIO_LED_GREEN, !digitalRead(GPIO_LED_GREEN));
+		}
 	}
 
-	// handle networking
-	int packet_code = read_parse_udp_packet();
+	// handle UDP messages
+	DoorCodes::Codes packet_code = read_parse_udp_packet();
 
-	if (packet_code == DoorCodes::Codes::NO_DATA) {
-		return;
+	switch (packet_code) {
+		
+		case DoorCodes::Codes::START_ALARM: {
+			Serial.print("[DEBUG] loop() - recieved packet START_ALARM\n");
+			start_alarm();
+			break;
+		}
+
+		case DoorCodes::Codes::STOP_ALARM: {
+			Serial.print("[DEBUG] loop() - recieved packet STOP_ALARM\n");
+			g_EVENT_CANCEL_ALARM = true;
+			break;
+		}
+
+		default:
+			break;
 	}
 
-	else if (packet_code == DoorCodes::Codes::STOP_ALARM) {
-		g_EVENT_CANCEL_ALARM = true;
-	}
-
-	else if (packet_code == DoorCodes::Codes::START_ALARM) {
-		alarm();
-	}
+	handle_alarm();
 }
